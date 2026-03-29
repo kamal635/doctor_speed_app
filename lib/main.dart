d2b9a4a0789e8876177e2ee7283d09c5bff866a4
Kamal Ibrahim <kokoangry51@gmail.com>
2026-03-29 12:04:51 +0300

    chore: initial flutter project

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const DoctorSpeedApp());
+}
+
+class DoctorSpeedApp extends StatelessWidget {
+  const DoctorSpeedApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
