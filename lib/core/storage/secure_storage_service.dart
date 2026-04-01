d91900254e33d0559c80ed57049834331799980a
Kamal Ibrahim <kokoangry51@gmail.com>
2026-04-01 10:28:11 +0300

    feat(core/storage): add secure storage wrapper

---
+++ lib/core/storage/secure_storage_service.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class SecureStorageService {
+  SecureStorageService({FlutterSecureStorage? storage})
+    : _storage = storage ?? const FlutterSecureStorage();
+
+  final FlutterSecureStorage _storage;
+
+  Future<void> write({required String key, required String value}) {
+    return _storage.write(key: key, value: value);
+  }
+
+  Future<String?> read(String key) {
+    return _storage.read(key: key);
+  }
+
+  Future<void> delete(String key) {
+    return _storage.delete(key: key);
+  }
+
+  Future<void> deleteAll() {
+    return _storage.deleteAll();
+  }
+
+  Future<bool> containsKey(String key) {
+    return _storage.containsKey(key: key);
+  }
+}
