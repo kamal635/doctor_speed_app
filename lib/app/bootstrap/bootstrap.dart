a1f8cb4ae91e4614a29c216d6122b577aab16b1e
Kamal Ibrahim <kokoangry51@gmail.com>
2026-04-10 16:14:18 +0300

    enforce portrait-only app orientation

--- lib/app/bootstrap/bootstrap.dart
+++ lib/app/bootstrap/bootstrap.dart
@@ -1,10 +1,13 @@
 import 'dart:async';
 
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
   final Widget app = await builder();
 
